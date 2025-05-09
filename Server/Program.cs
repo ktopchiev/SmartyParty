d88447ff76dd6037a9b5b5aebf75beccee5c23b4
Karol Topchiev <karoltopchiev@outlook.com>
2025-05-09 21:09:10 +0300

    .

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Server.Data;
-using Server.DTOs;
 using Server.Endpoints;
 using Server.Hubs;
-using Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
